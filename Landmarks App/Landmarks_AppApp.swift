d62fb80536f3f96c3f7d176f54458e3ed8dbb163
Blueturboguy07 <114044325+Blueturboguy07@users.noreply.github.com>
2025-08-01 15:02:44 -0500

    Initial Commit

---
+++ Landmarks App/Landmarks_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Landmarks_AppApp.swift
+//  Landmarks App
+//
+//  Created by Mann Bellani on 8/1/25.
+//
+
+import SwiftUI
+
+@main
+struct Landmarks_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
